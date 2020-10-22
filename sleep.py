1f343588ead3d690720db8ed45b7320f67e2b1a2
徐明皓 <xuminghao.118@bytedance.com>
2020-10-22 09:22:20 +0800

    Update sleep

--- sleep.py
+++ sleep.py
@@ -2,7 +2,7 @@ import time
 import torch
 
 while True:
-    x = torch.rand((2000, 2048)).cuda()
-    y = torch.rand((2000, 2048)).cuda()
+    x = torch.rand((4000, 2048)).cuda()
+    y = torch.rand((4000, 2048)).cuda()
     z = torch.mm(x, y.t()).mean()
     print('Results: ', z.item())
